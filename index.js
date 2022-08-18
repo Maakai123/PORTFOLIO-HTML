a74cae9af2ac5751880773fdc9cb6373446f9416
Maakai123 <maakaiprincewill@gmail.com>
2022-08-18 20:24:06 +0100

    first commit

---
+++ index.js
@@ -0,0 +1,11 @@
+// Can also be included with a regular script tag
+import Typed from 'typed.js';
+
+var options = {
+  strings: ['Maakai', 'Web3 Developer'],
+  typeSpeed: 100,
+  backspeed: 100,
+  loop:true
+};
+
+var typed = new Typed('.typing', options);
